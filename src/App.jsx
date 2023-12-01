b4334a5073c15e46ba93a6ceba5ab2ac73a51381
Юлия Пшеничная <yulenykaa@gmail.com>
2023-12-01 10:10:52 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import MaterialTableComponent from "./components/MaterialTableComponent";
-import data from "./data.json";
+import data from "./data/data.json";
 
 function App() {
   const [tableData, setTableData] = useState([]);
